f35802f79051f41a183eab0f06bac9c299db3b52
email4prasanth <email4prasanth@gmail.com>
2025-07-29 17:37:17 +0530

    sg and subnets are added

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -11,18 +11,19 @@ from infrastructure.secrets_stack import SecretsStack
 
 app = cdk.App()
 env_name = app.node.try_get_context("env") or "dev"
+prefix = f"testpy-{env_name}"
 
 # Create VPC stack
 vpc_stack = VPCStack(
     app, 
-    f"VPCStack-{env_name}",
+    f"{prefix}-vpc",
     environment=env_name,
     env=cdk.Environment(account='180294218712', region='us-east-1')
     )
 # Create Security Group stack
 sg_stack = SecurityGroupStack(
     app,
-    f"SecurityGroupStack-{env_name}",
+    f"{prefix}-sg",
     environment=env_name,
     vpc_id=vpc_stack.vpc.ref,
     env=cdk.Environment(account='180294218712', region='us-east-1')
@@ -31,7 +32,7 @@ sg_stack = SecurityGroupStack(
 # Create Secrets stack
 secrets_stack = SecretsStack(
     app, 
-    f"SecretsStack-{env_name}",
+    f"{prefix}-secrets",
     environment=env_name,
     env=cdk.Environment(account='180294218712', region='us-east-1')
 )
@@ -39,7 +40,7 @@ secrets_stack = SecretsStack(
 # Create RDS stack
 rds_stack = RDSStack(
     app,
-    f"RDSStack-{env_name}",
+    f"{prefix}-rds",
     environment=env_name,
     vpc=vpc_stack.vpc,  
     public_subnets=vpc_stack.public_subnets,
